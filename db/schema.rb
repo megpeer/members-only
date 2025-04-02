9561ec3ee16a966f3627f9aeac75f4720a28fd1a
megpeer <148973963+megpeer@users.noreply.github.com>
2025-04-02 09:27:42 -0400

    added post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_01_011707) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_02_132709) do
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
