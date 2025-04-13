7b92c355fcf5b2611b6a5d429f49cac37a4b0a38
megpeer <148973963+megpeer@users.noreply.github.com>
2025-04-12 22:42:03 -0400

    refined adding posts with user id attached, displayed with turbo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,30 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
   def index
    @post = Post.new
+   @posts = Post.all.order(created_at: :asc)
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    respond_to do |format|
+      if @post.save
+        format.turbo_stream
+      else
+        format.html do
+          flash[:post_errors] = @post.errors.full_messages
+          redirect_to root_path
+        end
+      end
+    end
   end
 
   def destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
